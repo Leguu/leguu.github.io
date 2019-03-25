63f17304fe2ed7707786dc95bb4ad70ce9c06cb8
leguu <33807061+Leguu@users.noreply.github.com>
2019-03-25 10:14:49 +0400

    code cleanup

--- dvd.js
+++ dvd.js
@@ -3,21 +3,18 @@ class DigitalVersatileDisc {
         this.canvas = canvas;
         this.context = canvas.getContext("2d");
 
-        this.direction = Math.random() * (2 * Math.PI);
         this.speed = 3;
-        this.update_speed();
+        this.update_speed(Math.random() * (2 * Math.PI));
 
         this.image = new Image;
         this.image.src = "https://upload.wikimedia.org/wikipedia/en/1/18/Dvd-video-logo.svg";
 
-        this.x_size = 100;
-        this.y_size = 100;
-
-        this.x = Math.random() * (this.canvas.width - this.x_size);
-        this.y = Math.random() * (this.canvas.height - this.y_size);
+        this.x_size = this.y_size = 100;
+        this.x = Math.random() * (canvas.width - this.x_size);
+        this.y = Math.random() * (canvas.height - this.y_size);
     }
 
-    run() {
+    draw_frame() {
         this.context.clearRect(this.x, this.y, this.x_size, this.y_size);
         this.update_position();
         this.context.drawImage(this.image, this.x, this.y, this.x_size, this.y_size);
@@ -26,20 +23,19 @@ class DigitalVersatileDisc {
     update_position() {
         if (this.x < 0 || this.x + this.x_size > this.canvas.width) {
             this.x -= this.x_speed;
-            this.direction = Math.PI - this.direction;
-            this.update_speed();
+            this.update_speed(Math.PI - this.direction);
         } else if (this.y < 0 || this.y + this.y_size > this.canvas.height) {
             this.y -= this.y_speed;
-            this.direction -= this.direction * 2;
-            this.update_speed();
+            this.update_speed(this.direction - (this.direction * 2));
         }
         this.x += this.x_speed;
         this.y += this.y_speed;
     }
 
-    update_speed() {
-        this.x_speed = Math.cos(this.direction) * this.speed;
-        this.y_speed = Math.sin(this.direction) * this.speed;
+    update_speed(direction) {
+        this.direction = direction;
+        this.x_speed = Math.cos(direction) * this.speed;
+        this.y_speed = Math.sin(direction) * this.speed;
     }
 }
 
