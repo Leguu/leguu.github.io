65dbe5cf9f3e21ed1ff2dded203fc5ebe48558a7
leguu <33807061+Leguu@users.noreply.github.com>
2019-03-26 14:14:09 +0400

    entity class and player class update

--- collision/collision.js
+++ collision/collision.js
@@ -1,19 +1,34 @@
-class Player {
-    constructor(canvas) {
+class Entity {
+    constructor(canvas, speed, direction, x, y, radius) {
         this.canvas = canvas;
         this.context = canvas.getContext("2d");
 
-        this.speed = 5;
-        this.update_bearing(0);
+        this.speed = speed;
+        this.update_bearing(direction);
 
-        this.x_size = this.y_size = 100;
-        this.x = Math.random() * (canvas.width - this.x_size);
-        this.y = Math.random() * (canvas.height - this.y_size);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
     }
 
     draw_frame() {
-        this.context.clearRect(this.x, this.y, this.x_size, this.y_size);
-        this.context.fillRect(this.x, this.y, this.x_size, this.y_size);
+        this.context.clearRect(
+            (this.x - this.radius) - 1,
+            (this.y - this.radius) - 1,
+            (this.radius * 2) + 2,
+            (this.radius * 2) + 2
+        );
+
+        this.update_position();
+
+        this.context.beginPath();
+        this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        this.context.fill();
+    }
+
+    update_position() {
+        this.x += this.x_speed;
+        this.y -= this.y_speed;
     }
 
     update_bearing(direction) {
@@ -22,3 +37,79 @@ class Player {
         this.y_speed = Math.sin(direction) * this.speed;
     }
 }
+
+
+class Player extends Entity {
+    constructor(canvas) {
+        super(canvas, 5, 0, 400, 300, 20);
+
+        this.keys_pressed = 0;
+
+        this.allowed = ["w", "a", "s", "d"];
+
+        this.pressed = {};
+        this.pressed["w"] = false;
+        this.pressed["a"] = false;
+        this.pressed["s"] = false;
+        this.pressed["d"] = false;
+
+        window.addEventListener('keydown', this.key_down.bind(this));
+        window.addEventListener('keyup', this.key_up.bind(this));
+    }
+
+    update_position() {
+        if(
+            !this.pressed["w"] &&
+            !this.pressed["a"] &&
+            !this.pressed["s"] &&
+            !this.pressed["d"]
+        ) { return };
+        this.update_bearing(this.direction);
+        super.update_position();
+    }
+
+    key_down(event) {
+        if(!this.allowed.includes(event.key)) { return };
+        if(this.pressed[event.key]) { return };
+
+        switch(event.key) {
+            case "w": this.direction += Math.PI / 2;        break;
+            case "a": this.direction += Math.PI;            break;
+            case "s": this.direction += 3 / 2 * Math.PI;    break;
+            case "d": this.pressed["w"] ? this.direction += 0 : this.direction += 2 * Math.PI;
+        };
+
+        this.pressed[event.key] = true;
+
+        this.keys_pressed++;
+        this.direction /= this.keys_pressed;
+
+        console.log(`KEY_DOWN: Pressed: ${this.keys_pressed}, Direction: ${this.direction}`);
+        console.log(`x_speed: ${this.x_speed}`);
+        console.log(`y_speed: ${this.y_speed}`);
+    }
+
+    key_up(event) {
+        if(!this.allowed.includes(event.key)) { return };
+
+        this.direction *= this.keys_pressed;
+        this.keys_pressed--;
+
+        switch(event.key) {
+            case "w": this.direction -= Math.PI / 2;        break;
+            case "a": this.direction -= Math.PI;            break;
+            case "s": this.direction -= (3 / 2) * Math.PI;  break;
+            case "d": this.pressed["w"] ? this.direction -= 0 : this.direction -= 2 * Math.PI;
+        };
+
+        this.pressed[event.key] = false;
+    }
+}
+
+let player = new Player(document.getElementById("collisionCanvas"));
+function loop() {
+    window.requestAnimationFrame(loop);
+    player.draw_frame();
+}
+
+loop();
